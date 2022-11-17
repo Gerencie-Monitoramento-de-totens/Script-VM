9ac51e067b46db3e3bc5002702900b0b5362cf68
GabrielaKubo <gabriela.kubo@sptech.school>
2022-11-17 20:20:08 -0300

    scrip vm

--- script.sh
+++ script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-sudo passwd ubuntu
-sudo apt update && sudo apt upgrade -y
+echo "Você gostaria de alterar a senha? (s/n)"
+read inst
+if [ \"$inst\" == \"s\" ];
+then
+	sudo passwd ubuntu
+else
+	echo "Prosseguindo..."
+fi
+	sudo apt update && sudo apt upgrade -y
 
 echo "Você gostaria de instalar a interface gráfica? (s/n)"
 read inst
@@ -8,7 +15,7 @@ if [ \"$inst\" == \"s\" ];
 then
 		sudo apt-get install xrdp lxde-core lxde tigervnc-standalone-server -y
 else
-		echo "Prosseguindo."
+		echo "Prosseguindo..."
 fi
 
 #saber versão java
@@ -31,28 +38,39 @@ else
 	if [ \"$inst\" == \"s\" ];
 	then
 		sudo apt install default-jre -y
+	else
+		echo "Prosseguindo..."
 	fi
 fi
 
 #Criando docker
-sudo apt install docker.io
-sudo systemctl start docker
-sudo systemctl enable docker
-# sudo docker –-version
-sudo docker ps
-# Pull ele está baixando o MYSQL.
-sudo docker pull mysql:5.7
-sudo docker images
-# Ele cria as configurações do mysql..
-# Caso não consiga executar o banco pelo java descomentar linha abaixo.
-sudo docker run -d -p 3306:3306 --name ContainerBD -e "MYSQL_DATABASE=gerencie" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
-# Aqui ele já começa a rodar o banco.
-# sudo docker exec -it ContainerBD bash
+docker --version
+if [ $? -eq 0 ];
+then
+	echo "Docker já está instalado"
+else
+	echo "Gostaria de instalar o Docker e Banco Gerencie! ? (s/n)"
+	read inst
+	if [ \"$inst\" == \"s\" ];
+	then
+		sudo apt install docker.io -y
 
+		echo "Iniciando Docker"
+		echo "Caminho: "
+		pwd
+		sudo systemctl start docker
+		sudo systemctl enable docker
+		sudo docker pull mysql:5.7
+		sudo docker images
+		sudo docker run -d -p 3306:3306 --name ContainerBD -e "MYSQL_DATABASE=gerencie" -e "MYSQL_ROOT_PASSWORD=urubu100" mysql:5.7
+
+		echo Banco de dados Criado com Sucesso!
+	fi
+fi
 
 #Criando jar executável
-cd .. 
-cd ..
-sudo git clone https://github.com/Gerencie-Monitoramento-de-totens/JAR.git
+git clone https://github.com/Gerencie-Monitoramento-de-totens/JAR.git
 cd JAR/gerencie/target
-java -jar gerencie-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar gerencie-1.0-SNAPSHOT.jar
+
+
